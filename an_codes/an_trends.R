d0f728759b993fb7a4f4de0b592663df3e030e26
Gerard Chung <gcsk1982@live.unc.edu>
2021-06-09 13:54:50 -0400

    analysis codes first commit

---
+++ an_codes/an_trends.R
@@ -0,0 +1,131 @@
+# NEWS ON FATHERS IN SINGAPORE 
+# Text-mining news in Singapore abt fathers in Singapore == 
+
+## Aim of do-file
+### Section A) # of articles over time
+### Section B) Save dataset for analysis in another do-file 
+
+
+rm(list=ls())
+
+library(stringr)
+library(dplyr)
+library(janitor)
+library(ggplot2)
+library(ggtext)
+library(ggrepel)
+
+load(file = "cr_data/news2.RData")
+
+# Plot number by year ####
+num_by_yr <- 
+    news2 %>%
+    filter(year != 2021) %>% 
+    group_by(year) %>% 
+    summarize(num_yr = n()) %>% 
+    ungroup()
+
+p <- ggplot(data = num_by_yr, mapping = aes(x = year, y = num_yr )) 
+p + geom_line(color= "#2a474b" , size = 1) +   #"steelblue"
+    labs(title = "Number of Articles",
+         subtitle = "1992 to 2020",
+         x = "",
+         y = "Number of articles each year",
+         caption = "gerardchung.com | Codes: https://github.com/gerardchung/social_work_news_singapore") +
+ #   scale_x_date(date_labels = "%Y", date_breaks = "2 year", limit=c(min, max)) +
+    geom_smooth(color = "#ec9914" ,size =2,  method = "lm" , se = T) +  # "indianred3"
+    scale_color_brewer(palette = "Dark2",  guide="none") +
+    theme_minimal() 
+
+p + geom_point() +
+    labs(title = "Number of Articles",
+         subtitle = "1992 to 2020",
+         x = "",
+         y = "Number of articles each year",
+         caption = "gerardchung.com | Codes: https://github.com/gerardchung/social_work_news_singapore") +
+    #   scale_x_date(date_labels = "%Y", date_breaks = "2 year", limit=c(min, max)) +
+    geom_smooth(color = "#ec9914" ,size =2,  method = "lm" , se = T) +  # "indianred3"
+    scale_color_brewer(palette = "Dark2",  guide="none") +
+    theme_minimal() 
+
+## Regression 
+
+lm.year_articles = glm(num_yr~as.numeric(year), family="poisson", data = num_by_yr)
+summary(lm.year_articles)
+
+# Number of articles by source (ST, Today, CNA) #####
+
+num_by_yr_source <- 
+    news2 %>% 
+    filter(year != 2021) %>% 
+    group_by(year, source) %>% 
+    summarize(num_yr = n()) %>% 
+    ungroup()
+
+p <- ggplot(data = num_by_yr_source, mapping = aes(x = year, y = num_yr , group = source, color = source))
+p + geom_point(alpha = .6, size =1.5) +
+    geom_smooth( size =1.2,  method = "lm" , se = F) +  # "indianred3"
+    scale_color_brewer(palette = "Set1",  guide="none") +  #Dark2 
+    scale_x_continuous(limits = c(1992, 2020), breaks = seq(1992, 2020, by = 4)) +
+    
+    labs(title = "Number of articles with 'father'-related phrases in titles",
+         subtitle = "In <b><span style = 'color: #E41A1C;'>**Straits Times**</span></b>, <b><span style = 'color: #377EB8;'>**TodayOnline**</span></b>, and <b><span style = 'color: #4DAF4A;'>**CNA**</span></b> from 1992-2020", 
+         x = "",
+         y = "Number of articles",
+         caption = c("@GerardChung | gerardchung.com | Codes: https://github.com/gerardchung/news_fathers", 
+                     "Note: father-related phrases include dad*, father*, paternal")) +
+
+  #  theme_minimal() + 
+    theme_classic(base_family = "Roboto Condensed") + 
+    theme(
+        rect = element_rect(fill = "#F5F5F5"),
+        panel.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+        plot.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+        plot.title = ggtext::element_markdown(size = 15),
+        plot.title.position = "plot",
+        plot.subtitle = element_markdown(family = "sans", size = 10, lineheight = 1.2),
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        strip.text = element_markdown(size = 12, face = "bold"),
+        strip.background = element_blank(),
+        axis.line.x = element_line(size = 0.5, colour = "gray20"),
+        axis.text.x = element_text(size = 9, color = "gray20"),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.y = element_text(size = 12, color = "gray20"),
+        plot.caption = element_text(hjust=c(1, 0))) +  # this line of code is to have two captions
+  
+    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "ST") %>%  filter(year %in% range(year)), 
+                             aes(label =  num_yr ),  # paste0(Importance)
+                             hjust = "left",
+                             fontface = "plain",
+                             family = "Roboto Condensed",
+                             size = 3.5,
+                             #   nudge_x = -.9,
+                             direction = "y")   +
+    # range will give the min and max values of year (you can change to num_by_yr_source)
+                                 
+    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "Today") %>% filter(year %in% range(year)), 
+                             aes(label =  num_yr ),  # paste0(Importance)
+                             hjust = "left",
+                             fontface = "plain",
+                             family = "Roboto Condensed",
+                             size = 3.5,
+                          #   nudge_y = 1.5,nudge_x = 1,
+                             direction = "y")  +
+    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "CNA") %>%  filter(year %in% range(year)), 
+                             aes(label =  num_yr ),  # paste0(Importance)
+                             hjust = "left",
+                             fontface = "plain",
+                             family = "Roboto Condensed",
+                             size = 3.5,
+                             nudge_x = .3,nudge_y = -.9,
+                             direction = "y") 
+
+# Colors from Set1 used in this graph
+library("RColorBrewer")
+brewer.pal(3,"Set1")
+red = "#E41A1C"
+blue = "#377EB8"
+green = "#4DAF4A"
+
---
+++ an_codes/an_wordcloud_title.R
@@ -0,0 +1,227 @@
+# NEWS ON FATHERS IN SINGAPORE 
+# Text-mining news in Singapore abt fathers in Singapore == 
+
+## Aim of do-file
+### Section A) Convert to Corpus and DFM
+### Section B) Word clouds
+### Section C) Frequency of word occurrences
+
+
+rm(list=ls())
+
+library(stringr)
+library(dplyr)
+library(janitor)
+library(quanteda)
+library(ggplot2)
+library(ggtext)
+library(ggrepel)
+
+load(file = "cr_data/news2.RData")
+
+# Section A) Convert to Corpus and DFM ####
+
+## Convert to corpus ==== 
+news_corpus <- corpus(news2,
+                      docid_field = "id",
+                      text_field = "title")
+
+## Convert to tokens -> then DFM ==== 
+news_tok <- 
+    tokens(news_corpus, 
+                remove_numbers = T,
+                remove_punct = T,
+                remove_symbols = T,
+                include_docvars = T, 
+                remove_separators = T
+                ) 
+#%>% 
+#    tokens_wordstem() 
+    
+#news_tok <- tokens_tolower(news_tok)
+mystopwords <- c("father", "fathers", 
+                 "dad", "dads", "daddy", "daddies", 
+                 "man", "paternal", 
+                 "singapore", "s", "s'pore",
+                 "says")
+            # why "says", "s" etc? These came from the wordcloud in the following
+            # and I exclude them since they are not useful to the meaning 
+
+news_dfm <- 
+    dfm(news_tok,
+                tolower = T) %>% 
+    dfm_remove(
+               c(phrase(mystopwords), stopwords())) %>% 
+    dfm_wordstem() 
+    # impt that word stemming comes after stop word removal. 
+  
+
+# Section B) Word Clouds ####
+library("quanteda.textplots")
+set.seed(100)
+#textplot_wordcloud(news_dfm)
+
+textplot_wordcloud(news_dfm, 
+                   max_words = 100, 
+                   min_count = 30,
+                   random_order = F, 
+                   #  rotation = 0.25, 
+                   color = RColorBrewer::brewer.pal(8, "Dark2"))  #
+
+set.seed(100)
+textplot_wordcloud(news_dfm, color = c("#459DE0", "#F53446"), max_words = 100,
+                   min_count = 20, random_order = F ) 
+
+
+str_view(news2$title, pattern = "says")
+str_view(news2$title, pattern = "\\bson\\b", match = T)
+
+
+# Section C) Frequency of word occurrences ####
+
+## Frequency plots using textstat_frequency ==== 
+# https://quanteda.io/articles/pkgdown/examples/plotting.html#frequency-plots-1
+# https://tutorials.quanteda.io/statistical-analysis/frequency/
+# https://quanteda.io/reference/textstat_frequency.html
+
+library("quanteda.textstats")
+
+## Colors from Set1 used in this graph ==== 
+library("RColorBrewer")
+brewer.pal(3,"Set1")
+red = "#E41A1C"
+  blue = "#377EB8"
+green = "#4DAF4A"
+
+## Create DF for textstat_freq ==== 
+features_plot <- textstat_frequency(news_dfm, n = 20)
+features_plot$feature <- with(features_plot, reorder(feature, docfreq)) 
+    # docfreq = how many docs the word appear in
+    # freq = how many times the word appear (can be once or more in a doc)
+
+## Plot the textstat_freq  ==== 
+ggplot(features_plot, aes(x = feature, y = docfreq)) +
+    geom_segment( aes(xend=feature, yend=0)) +
+    geom_point(color = blue, size = 3) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    coord_flip() + 
+    
+    labs(title = "Number of articles where the word appeared",
+         subtitle = "Top 20 words",
+         x = "Word",
+         y = "Number of articles",
+         caption = "@GerardChung | gerardchung.com | Codes: https://github.com/gerardchung/news_fathers") +
+    
+    theme_classic(base_family = "Roboto Condensed") + 
+    theme(
+        rect = element_rect(fill = "#F5F5F5"),
+        panel.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+        plot.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+        plot.title = ggtext::element_markdown(size = 16),
+        plot.title.position = "plot",
+        plot.subtitle = element_markdown(family = "Roboto Condensed", size = 14, lineheight = 1.2),
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        strip.text = element_markdown(size = 12, face = "bold"),
+        strip.background = element_blank(),
+        axis.line.x = element_line(size = 0.5, colour = "gray20"),
+        axis.text.x = element_text(size = 12, color = "gray20"),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.y = element_text(size = 12, color = "gray20")) +
+    
+    ggrepel::geom_label_repel(data = features_plot %>% filter(feature == "son"), 
+                             aes(label =  docfreq),  # paste0(Importance)
+                             hjust = "left",
+                             fontface = "plain",
+                             family = "Roboto Condensed",
+                             size = 3,
+                              nudge_x = -0.5, 
+                             box.padding = 0.01,
+                             direction = "y",
+                             
+                            ) +
+    ggrepel::geom_label_repel(data = features_plot %>% filter(feature == "daughter"), 
+                             aes(label =  docfreq),  # paste0(Importance)
+                             hjust = "left", 
+                             fontface = "plain",
+                             family = "Roboto Condensed",
+                             size = 3,
+                             nudge_x = -.4,nudge_y = 5.2, box.padding = 0.01, segment.curvature = 0.08,
+                            direction = "y")  -> freqplot
+  
+
+
+
+
+## See samples of titles for the top words ==== 
+str_view(news2$title, pattern = "\\bson\\b", match = T)
+son_text <- news2
+son_text$son <- str_detect(son_text$title, regex(pattern = "\\bson\\b", ignore_case = T))
+son_text <-
+  son_text %>% 
+  filter(son == T) %>% 
+  select(id, date, source, title, text, son )
+
+head(son_text$title, n = 10) 
+print(son_text$title) 
+
+## Sample text 1 
+son_text[son_text$title=="Commando son part of Best Combat Unit - just like his  dad",]
+# 2017-06-29 Todayonline
+## Sample text 2
+son_text[son_text$title=="Father  v son: 2; Dad , 96, wants entire compensation given to son, 70, for Hock Kee unit",]
+## Sample text 3
+son_text[son_text$title=="Father  and son among 109 drug offenders arrested in islandwide raids",]
+## Sample text 4
+son_text[son_text$title=="Like  father , like son, like grandsons",]
+## Sample text 5
+son_text[son_text$title=="Father  and son are like friends; Actor-host and single  dad  Jeff Wang says he spends most of his free time with his 13-year-old son",]
+
+
+
+
+freqplot + 
+  annotate("text", x=18, y=250, 
+           label= "'Commando son part of Best Combat Unit - just like his dad' 
+           - Straits Times, 2005-11-26",
+           size = 3.5,
+           family = "Roboto Condensed") +
+  annotate("text", x=15, y=230, 
+           label= "'Like father, like son, like grandsons' 
+           - ST, 2006-01-15",
+           size = 3.5,
+           family = "Roboto Condensed")  +
+  annotate("text", x=12, y=230, 
+           label= "'Father v son: Dad, 96, wants entire compensation given to son, 70, for Hock Kee unit' 
+           - TodayOnline, 2017-06-09",
+           size = 3.5,
+           family = "Roboto Condensed") + 
+  annotate("text", x=9, y=230, 
+           label= "'Father and son among 109 drug offenders arrested in islandwide raids' 
+           - Channel News Asia, 2019-07-12",
+           size = 3.5,
+           family = "Roboto Condensed") + 
+  annotate("text", x=6, y=230, 
+           label= "'Father and son are like friends; Actor-host and single dad Jeff Wang says 
+           he spends most of his free time with his 13-year-old son' 
+           - ST, 2021-05-31",
+           size = 3.5,
+           family = "Roboto Condensed")
+  # https://viz-ggplot2.rsquaredacademy.com/ggplot2-text-annotations.html
+
+
+ 
+# Feature-occurrence matrix ######
+#https://quanteda.io/articles/pkgdown/examples/twitter.html
+
+library("quanteda.textplots")
+news_fcm <- fcm(news_dfm)
+head(news_fcm)
+toptag <- names(topfeatures(news_dfm, 20))
+head(toptag, n = 10)
+
+top_fcm <- fcm_select(news_fcm, pattern = toptag)
+
+textplot_network(top_fcm, min_freq = 1, edge_alpha = 0.5, edge_size = 5)
+textplot_network(top_fcm, min_freq = 0.01, edge_color = "orange", edge_alpha = 0.8, edge_size = 1)
