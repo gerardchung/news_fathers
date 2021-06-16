3a7613db3773289f12ecfbcae9302d99acd8c003
Gerard Chung <gcsk1982@live.unc.edu>
2021-06-16 14:16:14 -0400

    final

--- an_codes/an_trends.R
+++ an_codes/an_trends.R
@@ -91,63 +91,68 @@ num_by_yr_source <-
 
 p <- ggplot(data = num_by_yr_source, mapping = aes(x = year, y = num_yr , group = source, color = source))
 p + geom_point(alpha = .6, size =1.5) +
-    geom_smooth( size =1.2,  method = "lm" , se = F) +  # "indianred3"
-    scale_color_brewer(palette = "Set1",  guide="none") +  #Dark2 
-    scale_x_continuous(limits = c(1992, 2020), breaks = seq(1992, 2020, by = 4)) +
-    
-    labs(title = "Number of articles with 'father'-related phrases in titles",
-         subtitle = "In <b><span style = 'color: #E41A1C;'>**Straits Times**</span></b>, <b><span style = 'color: #377EB8;'>**TodayOnline**</span></b>, and <b><span style = 'color: #4DAF4A;'>**CNA**</span></b> from 1992-2020", 
-         x = "",
-         y = "Number of articles",
-         caption = c("@GerardChung | gerardchung.com | Codes: https://github.com/gerardchung/news_fathers", 
-                     "Note: father-related phrases include dad*, father*, paternal")) +
-
-  #  theme_minimal() + 
-    theme_classic(base_family = "Roboto Condensed") + 
-    theme(
-        rect = element_rect(fill = "#F5F5F5"),
-        panel.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
-        plot.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
-        plot.title = ggtext::element_markdown(size = 15, face="bold"),
-        plot.title.position = "plot",
-        plot.subtitle = element_markdown(family = "Roboto Condensed", size = 13, lineheight = 1.2),
-        panel.grid.minor=element_blank(),
-        panel.grid.major=element_blank(),
-        strip.text = element_markdown(size = 12, face = "bold"),
-        strip.background = element_blank(),
-        axis.line.x = element_line(size = 0.5, colour = "gray20"),
-        axis.text.x = element_text(size = 9, color = "gray20"),
-        axis.line.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.y = element_text(size = 12, color = "gray20"),
-        plot.caption = element_text(hjust=c(1, 0))) +  # this line of code is to have two captions
+  geom_smooth( size =1.2,  method = "lm" , se = F) +  # "indianred3"
+  scale_color_brewer(palette = "Set1",  guide="none") +  #Dark2 
+  scale_x_continuous(limits = c(1992, 2020), breaks = seq(1992, 2020, by = 4)) +
   
-    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "ST") %>%  filter(year %in% range(year)), 
-                             aes(label =  num_yr ),  # paste0(Importance)
-                             hjust = "left",
-                             fontface = "plain",
-                             family = "Roboto Condensed",
-                             size = 3.5,
-                             #   nudge_x = -.9,
-                             direction = "y")   +
-    # range will give the min and max values of year (you can change to num_by_yr_source)
-                                 
-    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "Today") %>% filter(year %in% range(year)), 
-                             aes(label =  num_yr ),  # paste0(Importance)
-                             hjust = "left",
-                             fontface = "plain",
-                             family = "Roboto Condensed",
-                             size = 3.5,
-                          #   nudge_y = 1.5,nudge_x = 1,
-                             direction = "y")  +
-    ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "CNA") %>%  filter(year %in% range(year)), 
-                             aes(label =  num_yr ),  # paste0(Importance)
-                             hjust = "left",
-                             fontface = "plain",
-                             family = "Roboto Condensed",
-                             size = 3.5,
-                             nudge_x = .3,nudge_y = -.9,
-                             direction = "y") 
+  ylab("Number of articles") + 
+  xlab("Year") + 
+  
+  labs(title = "Number of news articles with 'fathers' in titles from 1992 to 2020",
+       subtitle = "In <b><span style = 'color: #E41A1C;'>**Straits Times**</span></b>, <b><span style = 'color: #377EB8;'>**TodayOnline**</span></b>, and <b><span style = 'color: #4DAF4A;'>**CNA**</span></b>",
+       caption = "Plot by **Gerard Chung** | gerardchung.com | Codes at **github.com/gerardchung/news_fathers**<br>Note: **'father'** in title include *dad*, *father*, *paternal*") +
+  
+  #  theme_minimal() + 
+  theme_classic(base_family = "Roboto Condensed") + 
+  theme(
+    rect = element_rect(fill = "#F5F5F5"),
+    panel.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+    plot.background = element_rect(fill = "#F5F5F5", color = "#F5F5F5"),
+    plot.title = ggtext::element_markdown(size = 16, face="bold"),
+    plot.title.position = "plot",
+    plot.subtitle = element_markdown(family = "Roboto Condensed", size = 14, lineheight = 1.2),
+    panel.grid.minor=element_blank(),
+    panel.grid.major=element_blank(),
+    strip.text = element_markdown(size = 12, face = "bold"),
+    strip.background = element_blank(),
+    axis.line.x = element_line(size = 0.5, colour = "gray20"),
+    axis.text.x = element_text(size = 9, color = "gray20"),
+    axis.line.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_text(size = 10, color = "gray20"),
+    plot.caption = element_markdown(hjust = 0, lineheight = 1.5 )
+  ) +  # this line of code is to have two captions
+  
+  ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "ST") %>%  filter(year %in% range(year)), 
+                           aes(label =  num_yr ),  # paste0(Importance)
+                           hjust = "left",
+                           fontface = "plain",
+                           family = "Roboto Condensed",
+                           size = 3.5,
+                           #   nudge_x = -.9,
+                           direction = "y")   +
+  # range will give the min and max values of year (you can change to num_by_yr_source)
+  
+  ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "Today") %>% filter(year %in% range(year)), 
+                           aes(label =  num_yr ),  # paste0(Importance)
+                           hjust = "left",
+                           fontface = "plain",
+                           family = "Roboto Condensed",
+                           size = 3.5,
+                           #   nudge_y = 1.5,nudge_x = 1,
+                           direction = "y")  +
+  ggrepel::geom_text_repel(data = num_by_yr_source %>% filter(source == "CNA") %>%  filter(year %in% range(year)), 
+                           aes(label =  num_yr ),  # paste0(Importance)
+                           hjust = "left",
+                           fontface = "plain",
+                           family = "Roboto Condensed",
+                           size = 3.5,
+                           nudge_x = .3,nudge_y = -.9,
+                           direction = "y") -> plot_final
+  ggsave("plots/1_trends_3sources.png", plot = plot_final, type = 'cairo', width = 8, height = 6.5, dpi = 300, units = "in", bg = "#F5F5F5")
+
+ggsave(filename = "plots/1_trends_3sources.png", width = 60, height = 40, units = "cm")
+
 
 # Colors from Set1 used in this graph
 library("RColorBrewer")
@@ -155,4 +160,5 @@ brewer.pal(3,"Set1")
 red = "#E41A1C"
 blue = "#377EB8"
 green = "#4DAF4A"
+
 
